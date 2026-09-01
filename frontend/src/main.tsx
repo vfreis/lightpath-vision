fb1069e723a2d701783a895adfa510c87f12a33c
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2026-09-01 18:51:27 -0300

    feat(frontend): load verified La Braciera brand overlay

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
+import './brand.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>)
